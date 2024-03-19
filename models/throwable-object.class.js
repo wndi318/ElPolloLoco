c2171b520c74079bef005436502777756ac8c7ac
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-03-19 18:48:56 +0100

    animate bottle

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,6 +1,24 @@
 class ThrowableObject extends MovableObject {
+    IMAGES_ROTATE = [
+        '../img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        '../img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        '../img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        '../img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ]
+
+    IMAGES_SPLASH = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+    ]
+
     constructor(x, y) {
-        super().loadImage('../img/6_salsa_bottle/salsa_bottle.png');
+        super().loadImage('../img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
+        this.loadImages(this.IMAGES_ROTATE);
+        this.loadImages(this.IMAGES_SPLASH);
         this.x = x;
         this.y = y;
         this.height = 70;
@@ -12,6 +30,7 @@ class ThrowableObject extends MovableObject {
         this.speedY = 30;
         this.applyGravity();
         setInterval(() => {
+            this.playAnimation(this.IMAGES_ROTATE);
             this.x += 10;
         }, 25);
     };
